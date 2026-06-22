245cd71de517fe033b3363a9f769402838c21505
Ado Tahirovic <sadullah.ado.tahirovic@bancstac.com>
2026-06-22 23:12:13 +0200

    fix all relative routes

--- js/product.js
+++ js/product.js
@@ -74,7 +74,7 @@ function renderProduct(p) {
             Dodaj u korpu
           </button>
         </div>
-        <a href="../cart.html" class="btn btn-outline btn--full" style="display:flex;">Idi u korpu</a>
+        <a href="./cart.html" class="btn btn-outline btn--full" style="display:flex;">Idi u korpu</a>
         ` : `<button class="btn btn-primary btn--full" disabled>Nema na zalihi</button>`}
         <div style="display:flex;align-items:center;gap:8px;padding:13px;background:var(--color-bg-soft);border-radius:var(--radius-md);font-size:0.8rem;color:var(--color-text-muted);">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" style="width:17px;height:17px;flex-shrink:0;"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" /></svg>
